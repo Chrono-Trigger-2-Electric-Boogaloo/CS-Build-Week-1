2f471ce269d3a02961ea87d92f275c14b9e07916
Krishan Nattar <krishan.nattar@gmail.com>
2020-02-05 16:06:21 -0800

    fixed models

--- adventure/models.py
+++ adventure/models.py
@@ -43,7 +43,8 @@ class Player(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     def initialize(self):
         if self.currentRoom == 0:
-            self.currentRoom = Room.objects.first().id
+            self.currentRoom = Room.objects.get(title="0,0,0").id
+            print(self.currentRoom)
             self.save()
     def room(self):
         try:
